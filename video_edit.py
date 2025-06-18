33dc999c795faad6368b46ea30730ee5c50e2e97
dungwoong <78326933+dungwoong@users.noreply.github.com>
2025-06-18 17:37:19 -0600

    move stuff to poc folder, add initial draft for video_edit

---
+++ video_edit.py
@@ -0,0 +1,15 @@
+from moviepy import ImageClip
+from moviepy.video.VideoClip import VideoClip
+
+# need to add animation ability
+class OverlayImage:
+    def __init__(self, video: VideoClip, rel_w: float, rel_h: float):
+        self.image = None
+        pass
+
+
+def add_enlarge_in(static_clip):
+    pass
+
+def add_fade_in(static_clip):
+    pass

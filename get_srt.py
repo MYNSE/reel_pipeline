61e717a42be5be2a206958d1a0f1b3a0f8ad0ab5
unknown <richardhe330@gmail.com>
2025-06-21 17:39:14 -0600

    half done chunking now

--- get_srt.py
+++ get_srt.py
@@ -3,4 +3,5 @@ import whisper
 
 model = whisper.load_model("base")
 results = model.transcribe("griffin.mp4", word_timestamps=True, fp16=False) # Transcribe the audio with word timestamps
-result_to_srt_vtt(results, "input")
+#results = model.transcribe("temp.mp3", word_timestamps=True, fp16=False) # works on mp3
+result_to_srt_vtt(results, "temp")
